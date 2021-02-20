504dc5efcdf15bca999d808ddd58da4784d983a5
EduardoFerreira1983 <eduardo.ferreira1983@gmail.com>
2021-02-19 21:52:24 -0300

    Criação Inicial -  Adaptação do Template Notus-React e inclusão do TinyMCE Editor

---
+++ Projetos/pacto-notus React/src/views/auth/Login.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  return (
+    <>
+      <div className="container mx-auto px-4 h-full">
+        <div className="flex content-center items-center justify-center h-full">
+          <div className="w-full lg:w-4/12 px-4">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
+              <div className="rounded-t mb-0 px-6 py-6">
+              </div>
+              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                <div className="text-gray-500 text-center mb-3 font-bold">
+                  <small>Entre com suas credenciais</small>
+                </div>
+                <form>
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className="px-3 py-3 placeholder-gray-400 
+                      text-gray-700 bg-white rounded text-sm shadow 
+                      focus:outline-none focus:shadow-outline w-full 
+                      ease-linear transition-all duration-150"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <a href = "/admin"> 
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Senha
+                      </label>
+                      <input
+                        type="password"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+                        placeholder="senha"
+                      />
+                    </div>
+                  </a>
+                  <div>
+                    <label className="inline-flex items-center cursor-pointer">
+                      <input
+                        id="customCheckLogin"
+                        type="checkbox"
+                        className="form-checkbox text-gray-800 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                      />
+                      <span className="ml-2 text-sm font-semibold text-gray-700">
+                        Lembrar-me
+                      </span>
+                    </label>
+                  </div>
+                  <div className="text-center mt-6">
+                    <Link to="/admin">
+                    <button
+                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="button"
+                    >
+                      Acessar
+                    </button>
+                    </Link>
+                  </div>
+                  <div className="flex flex-wrap mt-6 relative">
+                    <div className="w-1/2 text-gray-100">
+                      <small>Esqueceu a Senha?</small>
+                    </div>
+                   <div className="w-1/2 text-right">
+                      <Link to="/auth/register" className="text-gray-100">
+                        <small>Criar uma nova conta</small>
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
